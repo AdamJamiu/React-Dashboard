f8f178f8aaef990cec4932996553b4a0b69e7620
lil-armstrong <oebong1@gmail.com>
2021-12-21 14:29:48 +0100

    Additions - Login page - Forgot password page - Registration page - styled components

--- src/screens/regular/pages/Auth/index.js
+++ src/screens/regular/pages/Auth/index.js
@@ -1,6 +1,7 @@
 import LoginPage from "./login.page";
 import RegisterPage from "./register.page";
-
+import ForgotPassword from "./forgot.page";
 export default Object.assign(LoginPage, {
-  RegisterPage
+  RegisterPage,
+  ForgotPassword
 })
--- src/screens/regular/pages/Auth/login.page.js
+++ src/screens/regular/pages/Auth/login.page.js
@@ -1,3 +1,73 @@
+import { routeMapper } from "../../routes";
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
-  return <>You are in the login page</>
+  return (
+    <section className="container mt-[80px] max-w-[731px]">
+      <header>
+        <h2
+          className="text-onSurface-main"
+          style={{
+            fontStyle: "normal",
+            fontWeight: 500,
+            fontSize: "36px",
+            lineHeight: "44px",
+            textTransform: "capitalize",
+          }}
+        >
+          Welcome to Bybit
+        </h2>
+        <p
+          className="text-secondary-main"
+          style={{
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: 16,
+            lineHeight: "28px",
+          }}
+        >
+          Welcome back! Log In now to start trading
+        </p>
+      </header>
+
+      <div className="mt-[16px] flex rounded-[27px] bg-opacity-[0.1] bg-success-main">
+        {/* PADLOCK ICON */}
+        <span></span>
+        <span>https://www.bybit.com</span>
+      </div>
+
+      <div>
+        <ul>
+          <li>Email</li>
+          <li>Mobile</li>
+        </ul>
+
+        <section>
+          <form action="">
+            <label htmlFor="">
+              <span>Username or email address*</span>
+            </label>
+          </form>
+        </section>
+
+        <section>
+          <form action="">
+            <section>
+              <label htmlFor="">
+                <span>Password*</span>
+              </label>
+            </section>
+
+            <section className="flex justify-between">
+              <label htmlFor="">
+                <input type="checkbox" name="remmeber" id="remember" />
+              </label>
+
+              <Link to={routeMapper.forgot}>Forgot Your Password?</Link>
+            </section>
+          </form>
+        </section>
+      </div>
+    </section>
+  );
 }
